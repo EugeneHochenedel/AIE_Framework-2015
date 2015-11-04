472bdf328abe52b73f8b3b43d33c7331d829c8aa
SilentTremor <eugeneatschool@gmail.com>
2015-11-04 09:40:55 -0600

    Proof of Math
    Used the Vector Math library I linked to the project.

--- Your Project/GameLoop.cpp
+++ Your Project/GameLoop.cpp
@@ -40,55 +40,41 @@ void GameLoop::LateUpdate()
 
 }
 
-//int iX = 800;
-//int iY = 450;
-int iRadius = 200;
-int iQuality = 50;
-int iRed = 0;
-int iGreen = 255;
-int iBlue = 255;
-int iAlpha = 150;
+System::Point2D<int> Middle{ 800, 450 };
+System::Color<int> ColorCode{ 192, 192, 192, 150 };
 
 void GameLoop::Draw()
 {
 	// Objects are drawn in a painter's layer fashion meaning the first object drawn is on the bottom, and the last one drawn is on the top
 	// just like a painter would paint onto a canvas
 
-	Graphics::DrawRect({ 400, 400 }, { 450, 400 }, { 160, 65, 255, 255 });
+	Graphics::DrawRect({ 400, 400 }, { 400, 400 }, { 160, 65, 255, 255 });
 	Graphics::DrawRect({ 250, 500 }, { 1000, 200 }, { 0, 255, 0, 255 });
 
 	Graphics::DrawLine({ 10, 10 }, { 100, 100 }, { 255, 255, 255, 255 });
 	Graphics::DrawPoint({ 5, 5 }, { 255, 255, 255, 255 });
 
 	Graphics::DrawRing({ 140, 140 }, 50, 25, { 50, 0, 200, 255 });
-	Graphics::DrawCircle({ 800, 450 }, 200, 50, { 0, 255, 255, 150 });
+	Graphics::DrawCircle({ Middle }, 200, 50, { ColorCode });
 }
-
-struct System::Point2D<int> Directions;
-struct System::Size2D<int> Sizes;
-struct System::Color<int> Shades;
 
 void GameLoop::OnKeyDown(const SDL_Keycode ac_sdlSym, const Uint16 ac_uiMod, const SDL_Scancode ac_sdlScancode)
 {
 	
 	switch (ac_sdlSym)
 	{
-	case SDLK_w: Directions.Y -= 5; break;
-	case SDLK_s: Directions.Y += 5; break;
-	case SDLK_d: Directions.X += 5; break;
-	case SDLK_a: Directions.X -= 5; break;
-	case SDLK_UP: Sizes.W += 5; break;
-	case SDLK_DOWN: Sizes.H -= 5; break;
-	case SDLK_RIGHT: iQuality += 1; break;
-	case SDLK_LEFT: iQuality -= 1; break;
-	case SDLK_HOME: iRed += 1; break;
-	case SDLK_BACKSPACE: iRed -= 1; break;
-	case SDLK_PAGEUP: iGreen += 1; 	break;
-	case SDLK_INSERT: iGreen -= 1; break;
-	case SDLK_END: iBlue += 1; break;
-	case SDLK_RETURN: iBlue -= 1; break;
-	case SDLK_PAGEDOWN: iAlpha += 1; break;
-	case SDLK_DELETE: iAlpha -= 1; break;
+	case SDLK_w: Middle.Y -= 5; break;
+	case SDLK_s: Middle.Y += 5; break;
+	case SDLK_d: Middle.X += 5; break;
+	case SDLK_a: Middle.X -= 5; break;
+	case SDLK_HOME: ColorCode.Red += 1; break;
+	case SDLK_END: ColorCode.Red -= 1; break;
+	case SDLK_PAGEUP: ColorCode.Green += 1; 	break;
+	case SDLK_PAGEDOWN: ColorCode.Green -= 1; break;
+	case SDLK_BACKSPACE: ColorCode.Blue += 1; break;
+	case SDLK_RETURN: ColorCode.Blue -= 1; break;
+	case SDLK_INSERT: ColorCode.Alpha += 1; break;
+	case SDLK_DELETE: ColorCode.Alpha -= 1; break;
 	case SDLK_ESCAPE: m_bRunning = false; break; // End the loop
 
 	default: printf("%s\n",SDL_GetKeyName(ac_sdlSym)); break;
@@ -98,22 +84,19 @@ void GameLoop::OnKeyUp(const SDL_Keycode ac_sdlSym, const Uint16 ac_uiMod, const
 {
 	switch (ac_sdlSym)
 	{
-	case SDLK_w: Directions.Y = 450; break;
-	case SDLK_s: Directions.Y = 450; break;
-	case SDLK_d: Directions.X = 800; break;
-	case SDLK_a: Directions.X = 800; break;
-	case SDLK_UP: iRadius = 200; break;
-	case SDLK_DOWN: iRadius = 200; break;
-	case SDLK_RIGHT: iQuality = 50; break;
-	case SDLK_LEFT: iQuality = 50; break;
-	case SDLK_HOME: iRed = 0; break;
-	case SDLK_BACKSPACE: iRed = 0; break;
-	case SDLK_PAGEUP: iGreen = 255; break;
-	case SDLK_INSERT: iGreen = 255; break;
-	case SDLK_END: iBlue = 255; break;
-	case SDLK_RETURN: iBlue = 255; break;
-	case SDLK_PAGEDOWN: iAlpha = 150; break;
-	case SDLK_DELETE: iAlpha = 150; break;
+	/*case SDLK_w: iY = 450; break;
+	case SDLK_s: iY = 450; break;
+	case SDLK_d: iX = 800; break;
+	case SDLK_a: iX = 800; break;
+	case SDLK_HOME: iRed = 192; break;
+	case SDLK_END: iRed = 192; break;
+	case SDLK_PAGEUP: iGreen = 192; 	break;
+	case SDLK_PAGEDOWN: iGreen = 192; break;
+	case SDLK_BACKSPACE: iBlue = 192; break;
+	case SDLK_RETURN: iBlue = 192; break;
+	case SDLK_INSERT: iAlpha = 150; break;
+	case SDLK_DELETE: iAlpha = 150; break;*/
+	default: break;
 	}
 }
 void GameLoop::OnExit()
--- Your Project/Source.cpp
+++ Your Project/Source.cpp
@@ -7,6 +7,12 @@
 
 int wmain()
 {
+	Vectors<int> Vec1(5, 5);
+	Vectors<int> Vec2(6, 8);
+	Vectors<int> Vec(0, 0);
+	Vec = Addition(Vec1, Vec2);
+	cout << Vec.tX << ", " << Vec.tY << "\n\n";
+
 	Graphics::Init();
 
 	Graphics::NewWindow({ 1600, 900 }, false, { 1600, 900 }, "Graphics Engine");
